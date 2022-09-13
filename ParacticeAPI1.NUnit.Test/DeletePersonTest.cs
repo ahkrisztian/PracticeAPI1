e9659a534ee38d06f060cc7593c051ab4cdcfaa1
ahkrisztian <lunarcsik.krisztian@gmail.com>
2022-09-13 13:39:03 +0200

    Update and Delete Person Test

---
+++ ParacticeAPI1.NUnit.Test/DeletePersonTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeAPI1.Models;
+using PracticeAPI1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParacticeAPI1.NUnit.Test
+{
+    public class DeletePersonTest
+    {
+        private AppDbContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=PracticeAPI1; Integrated Security=True;Connect Timeout=30;" +
+                "Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False").Options);
+
+            var person = new Person()
+            {
+                FirstName = "Paul",
+                LastName = "Benet",
+                Addresses = new List<Address>()
+                {
+                    new Address()
+                    {
+                        AddressLine = "Kensigton Str. 1",
+                        City = "Baltimore",
+                        ZipCode = "23487",
+                        Country = "USA"
+                    },
+                    new Address()
+                    {
+                        AddressLine = "Jamaica Str. 11",
+                        City = "Baltimore",
+                        ZipCode = "23456",
+                        Country = "USA"
+                    }
+                }
+            };
+
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+        }
+
+        [Test]
+        public void DeletePerson()
+        {
+            var person = _context.Persons.Single(p => p.FirstName == "Paul" && p.LastName == "Benet");
+
+            var personId = person.Id;
+
+            _context.Persons.Remove(person);
+            _context.SaveChanges();
+
+            var found = _context.Persons.SingleOrDefault(p => p.FirstName == "Paul" && p.LastName == "Benet");
+            Assert.IsNull(found);
+
+            var address = _context.Address.Where(x => x.PersonId == personId);
+            Assert.AreEqual(0, address.Count());
+        }
+    }
+}
---
+++ ParacticeAPI1.NUnit.Test/UpdatePersonTest.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PracticeAPI1.Models;
+using PracticeAPI1.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParacticeAPI1.NUnit.Test
+{
+    public class UpdatePersonTest
+    {
+        private AppDbContext _context;
+
+        private int personId;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new AppDbContext(new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Database=PracticeAPI1; Integrated Security=True;Connect Timeout=30;" +
+                "Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False").Options);
+
+            var person = new Person()
+            {
+                FirstName = "Kemp",
+                LastName = "Boris",
+                Addresses = new List<Address>()
+                {
+                    new Address()
+                    {
+                        AddressLine = "Polker Str. 100",
+                        City = "Cleveland",
+                        ZipCode = "75487",
+                        Country = "USA"
+                    },
+                    new Address()
+                    {
+                        AddressLine = "Bourbon Str. 166",
+                        City = "Cleveland",
+                        ZipCode = "75440",
+                        Country = "USA"
+                    }
+
+                }
+            };
+
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+
+            personId = person.Id;
+        }
+
+        [Test]
+        public void UpdatePersonWithAddress()
+        {
+            var person = _context.Persons.Single(p => p.Id == personId);
+
+            string firstName = "Jospeh";
+            person.FirstName = firstName;
+
+            string country = "Germany";
+
+            var address = person.Addresses.First();
+            address.Country = country;
+
+            _context.SaveChanges();
+
+            var updatedperson = _context.Persons.Single(p => p.Id == personId);
+
+            Assert.AreEqual(firstName, updatedperson.FirstName);
+
+            Assert.AreEqual(country, updatedperson.Addresses.First().Country);
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            var person = _context.Persons.Single(p => p.Id == personId);
+
+            _context.Persons.Remove(person);
+            _context.SaveChanges();
+        }
+
+    }
+}
